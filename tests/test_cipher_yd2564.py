181d1714014f60af5ceb5c7a8ec84cab18adeaa5
yifeitung <yifeidong95@gmail.com>
2020-11-11 22:28:07 -0500

    add test

--- tests/test_cipher_yd2564.py
+++ tests/test_cipher_yd2564.py
@@ -1,5 +1,10 @@
 from cipher_yd2564 import __version__
 from cipher_yd2564 import cipher_yd2564
 
+
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_negative_shift():
+    assert cipher_yd2564.cipher('the', -1, encrypt=True) == 'sgd'
